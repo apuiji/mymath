3fb725661c4d26105224eeae27f16110e9b0483c
apuiji <2330683684@qq.com>
2024-02-26 18:29:56 +0800

    a

--- vector.hh
+++ vector.hh
@@ -9,15 +9,15 @@ namespace zlt::mymath {
   struct Vector: std::array<T, N> {
     Vector() = default;
     Vector(std::array<T, N> &&array) noexcept: std::array<T, N>(std::move(array)) {}
-    template<FloatingPoint U>
-    inline Vector<N, T> &operator =(const Vector<N, U> &a) noexcept;
-    template<FloatingPoint U>
-    inline constexpr operator Vector<N, U>() const noexcept;
+    template<size_t M, FloatingPoint U>
+    inline Vector<N, T> &operator =(const Vector<M, U> &a) noexcept;
+    template<size_t M, FloatingPoint U>
+    inline constexpr operator Vector<M, U>() const noexcept;
   };
 
   // add/sub begin
   template<size_t N, FloatingPoint T, FloatingPoint U, size_t ...I>
-  static constexpr auto add(const Vector<N, T> &a, const Vector<N, U> &b, std::index_sequence<I...>) noexcept {
+  constexpr auto add(const Vector<N, T> &a, const Vector<N, U> &b, std::index_sequence<I...>) noexcept {
     return Vector<N, HigherPrecision<T, U>>({ (a[I] + b[I])... });
   }
 
@@ -27,7 +27,7 @@ namespace zlt::mymath {
   }
 
   template<size_t N, FloatingPoint T, FloatingPoint U, size_t ...I>
-  static constexpr auto sub(const Vector<N, T> &a, const Vector<N, U> &b, std::index_sequence<I...>) noexcept {
+  constexpr auto sub(const Vector<N, T> &a, const Vector<N, U> &b, std::index_sequence<I...>) noexcept {
     return Vector<N, HigherPrecision<T, U>>({ (a[I] - b[I])... });
   }
 
@@ -39,7 +39,7 @@ namespace zlt::mymath {
 
   // negative begin
   template<size_t N, FloatingPoint T, size_t ...I>
-  static constexpr auto neg(const Vector<N, T> &a, std::index_sequence<I...>) noexcept {
+  constexpr auto neg(const Vector<N, T> &a, std::index_sequence<I...>) noexcept {
     return Vector<N, T>({ -a[I]... });
   }
 
@@ -51,7 +51,7 @@ namespace zlt::mymath {
 
   // mul/div begin
   template<size_t N, FloatingPoint T, FloatingPoint U, size_t ...I>
-  static constexpr auto mul(const Vector<N, T> &a, U b, std::index_sequence<I...>) noexcept {
+  constexpr auto mul(const Vector<N, T> &a, U b, std::index_sequence<I...>) noexcept {
     return Vector<N, HigherPrecision<T, U>>({ (a[I] * b)... });
   }
 
@@ -66,7 +66,7 @@ namespace zlt::mymath {
   }
 
   template<size_t N, FloatingPoint T, FloatingPoint U, size_t ...I>
-  static constexpr auto div(const Vector<N, T> &a, U b, std::index_sequence<I...>) noexcept {
+  constexpr auto div(const Vector<N, T> &a, U b, std::index_sequence<I...>) noexcept {
     return Vector<N, HigherPrecision<T, U>>({ (a[I] / b)... });
   }
 
@@ -95,31 +95,101 @@ namespace zlt::mymath {
   // equals end
 
   // assign and cast begin
-  template<size_t N, FloatingPoint T, FloatingPoint U, size_t ...I>
-  static int assign(Vector<N, T> &dest, const Vector<N, U> &src, std::index_sequence<I...>) noexcept {
+  template<size_t N, FloatingPoint T, size_t M, FloatingPoint U, size_t ...I>
+  int assign(Vector<N, T> &dest, const Vector<M, U> &src, std::index_sequence<I...>) noexcept {
     ((dest[I] = (T) src[I]), ...);
     return 0;
   }
 
+  template<size_t N, FloatingPoint T, size_t M, FloatingPoint U>
+  static inline int assign(Vector<N, T> &dest, const Vector<M, U> &src) noexcept {
+    return assign(dest, src, std::make_index_sequence<N < M ? N : M>());
+  }
+
   template<size_t N, FloatingPoint T>
-  template<FloatingPoint U>
-  inline Vector<N, T> &Vector<N, T>::operator =(const Vector<N, U> &a) noexcept {
-    assign(*this, a, std::make_index_sequence<N>());
+  template<size_t M, FloatingPoint U>
+  inline Vector<N, T> &Vector<N, T>::operator =(const Vector<M, U> &a) noexcept {
+    assign(*this, a);
     return *this;
   }
 
   template<size_t N, FloatingPoint T>
-  template<FloatingPoint U>
-  inline constexpr Vector<N, T>::operator Vector<N, U>() const noexcept {
-    Vector<N, U> a;
-    assign(a, *this, std::make_index_sequence<N>());
+  template<size_t M, FloatingPoint U>
+  inline constexpr Vector<N, T>::operator Vector<M, U>() const noexcept {
+    Vector<M, U> a;
+    assign(a, *this);
     return a;
   }
   // assign and cast end
 
-  // norm begin
+  // complex assign begin
+  template<size_t N, FloatingPoint T, FloatingPoint U, size_t ...I>
+  int addAssign(Vector<N, T> &a, const Vector<N, U> &b, std::index_sequence<I...>) noexcept {
+    ((a[I] += b[I]), ...);
+    return 0;
+  }
+
+  template<size_t N, FloatingPoint T, FloatingPoint U>
+  static inline auto &operator +=(Vector<N, T> &a, const Vector<N, U> &b) noexcept {
+    addAssign(a, b, std::make_index_sequence<N>());
+    return a;
+  }
+
+  template<size_t N, FloatingPoint T, FloatingPoint U, size_t ...I>
+  int subAssign(Vector<N, T> &a, const Vector<N, U> &b, std::index_sequence<I...>) noexcept {
+    ((a[I] -= b[I]), ...);
+    return 0;
+  }
+
+  template<size_t N, FloatingPoint T, FloatingPoint U>
+  static inline auto &operator -=(Vector<N, T> &a, const Vector<N, U> &b) noexcept {
+    subAssign(a, b, std::make_index_sequence<N>());
+    return a;
+  }
+
+  template<size_t N, FloatingPoint T, FloatingPoint U, size_t ...I>
+  int mulAssign(Vector<N, T> &a, U b, std::index_sequence<I...>) noexcept {
+    ((a[I] *= b), ...);
+    return 0;
+  }
+
+  template<size_t N, FloatingPoint T, FloatingPoint U>
+  static inline auto &operator *=(Vector<N, T> &a, U b) noexcept {
+    mulAssign(a, b, std::make_index_sequence<N>());
+    return a;
+  }
+
+  template<size_t N, FloatingPoint T, FloatingPoint U>
+  int divAssign(Vector<N, T> &a, U b, std::index_sequence<I...>) noexcept {
+    ((a[I] /= b), ...);
+    return 0;
+  }
+
+  template<size_t N, FloatingPoint T, FloatingPoint U>
+  static inline auto &operator /=(Vector<N, T> &a, U b) noexcept {
+    divAssign(a, b, std::make_index_sequence<N>());
+    return a;
+  }
+  // complex assign end
+
+  // dot/norm begin
+  template<size_t N, FloatingPoint T, FloatingPoint U, size_t ...I>
+  constexpr auto dot2(const Vector<N, T> &a, const Vector<N, U> &b, std::index_sequence<I...>) noexcept {
+    return ((a[I] * b[I]) + ...);
+  }
+
+  template<size_t N, FloatingPoint T, FloatingPoint U>
+  static inline constexpr auto dot2(const Vector<N, T> &a, const Vector<N, U> &b) noexcept {
+    return dot2(a, b, std::make_index_sequence<I...>());
+  }
+
+  template<size_t N, FloatingPoint T, FloatingPoint U>
+  static inline constexpr auto dot(const Vector<N, T> &a, const Vector<N, U> &b) noexcept {
+    return std::sqrt(dot2(a, b));
+  }
+
   template<size_t N, FloatingPoint T, size_t ...I>
-  static constexpr auto norm2(const Vector<N, T> &a, std::index_sequence<I...>) noexcept {
+  constexpr auto norm2(const Vector<N, T> &a, std::index_sequence<I...>) noexcept {
     return ((a[I] * a[I]) + ...);
   }
 
@@ -132,5 +202,18 @@ namespace zlt::mymath {
   static inline constexpr auto norm(const Vector<N, T> &a) noexcept {
     return std::sqrt(norm2(a));
   }
-  // norm end
+  // dot/norm end
+
+  template<size_t N, FloatingPoint = double>
+  using Point = Vector<N, FloatingPoint>;
+
+  template<size_t N, FloatingPoint T, FloatingPoint U>
+  static inline constexpr auto distance2(const Point<N, T> &a, const Point<N, U> &b) noexcept {
+    return norm2(b - a);
+  }
+
+  template<size_t N, FloatingPoint T, FloatingPoint U>
+  static inline constexpr auto distance(const Point<N, T> &a, const Point<N, U> &b) noexcept {
+    return std::sqrt(distance2(a, b));
+  }
 }
